cca625c4e97a27a8a72b712239514807af6b016f
lxy <617432954@qq.com>
2019-08-16 17:40:43 +0800

    图片坐标自适应

--- ConferencesManagement-master/src/pages/manage/components/floor/floor11.vue
+++ ConferencesManagement-master/src/pages/manage/components/floor/floor11.vue
@@ -5,8 +5,8 @@
     <div id="content1" style="margin:20px 10px;border:1px solid #005BAC">
       <img src="../../../../assets/images/floor/tu1.jpg" alt="园区" usemap="#Map1"/>
       <map name="Map1" id="Map1">
-      <area alt="" title="" href="/floor12" shape="poly" coords="2,3,4,119,299,118,298,2" />
-      </map>
+        <area alt="dalou1" title="dalou2" href="/floor12" shape="poly" coords="424,363,584,317,639,369,640,409,474,472,426,395" target="_self"
+              id="hot1" class="hot1" onmouseover="ahover('a1')"/>      </map>
     </div>
   </div>
 </template>
@@ -24,8 +24,57 @@
       };
     },
     methods: {
+      ahover(id){
+        alert(document.getElementById(id).className);
+      },
 
-    }
+      adjust() {
+        var timeout = null;//onresize触发次数过多，设置定时器
+        window.onresize = function () {
+          clearTimeout(timeout);
+          timeout = setTimeout(function () { window.location.reload(); }, 100);//页面大小变化，重新加载页面以刷新MAP
+        }
+        var map = document.getElementById("Map1");
+        var element = map.childNodes;
+        var itemNumber = element.length / 2;
+        for (var i = 0; i < itemNumber - 1; i++) {
+          var item = 2 * i + 1;
+          var oldCoords = element[item].coords;
+          var newcoords = this.adjustPosition(oldCoords);
+          element[item].setAttribute("coords", newcoords);
+        }
+        var test = element;
+      },
+
+      //调整MAP中坐标
+      adjustPosition(position) {
+        var pageWidth = document.body.clientWidth;//获取页面宽度
+        var pageHeith = document.body.clientHeight;//获取页面高度
+
+        var imageWidth = 1160;    //图片的长宽
+        var imageHeigth = 990;
+
+        var each = position.split(",");
+        //获取每个坐标点
+        for (var i = 0; i < each.length; i++) {
+          each[i] = Math.round(parseInt(each[i]) * pageWidth / imageWidth).toString();//x坐标
+          i++;
+          each[i] = Math.round(parseInt(each[i]) * pageHeith / imageHeigth).toString();//y坐标
+        }
+        //生成新的坐标点
+        var newPosition = "";
+        for (var i = 0; i < each.length; i++) {
+          newPosition += each[i];
+          if (i < each.length - 1) {
+            newPosition += ",";
+          }
+        }
+        return newPosition;
+      }
+    },
+    mounted(){
+      this.adjust();
+    },
   }
 </script>
 <style lang="stylus" scoped>
--- ConferencesManagement-master/src/router/index.js
+++ ConferencesManagement-master/src/router/index.js
@@ -15,7 +15,7 @@ import Already from '../pages/manage/components/Already'
 import Floor11 from '../pages/manage/components/floor/floor11'
 import Floor12 from '../pages/manage/components/floor/floor12'
 import Floor13 from '../pages/manage/components/floor/floor13'
-import Meetingroom from '../pages/manage/components/echarts/Meetingroom'
+import Meetingroom from '../pages/manage/components/echarts/meetingroom'
 import Order from '../pages/manage/components/echarts/Order'
 Vue.use(Router)
 
@@ -72,15 +72,15 @@ export default new Router({
           component: Rearch,
           meta: 'search',
         },
-        {path: '/floor11', name: 'Floor11', component: Floor11,meta:{breadNumber:2}},
-        {path: '/floor12', name: 'Floor12', component: Floor12,meta:{breadNumber:3}},
-        {path: '/floor13', name: 'Floor13', component: Floor13,meta:{breadNumber:4}},
+        {path: '/floor11', name: 'Floor11', component: Floor11,meta:{breadNumber:1}},
+        {path: '/floor12', name: 'Floor12', component: Floor12,meta:{breadNumber:2}},
+        {path: '/floor13', name: 'Floor13', component: Floor13,meta:{breadNumber:3}},
 
         {path: '/already', name: 'Already', component: Already},
 
         {path: '/reserve', name: 'Reserve', component: Reserve},
-        {path: '/meetingroom', name: 'Meetingroom', component: Meetingroom,meta:{breadNumber:2}},
-        {path: '/order', name: 'Order', component: Order,meta:{breadNumber:3}},
+        {path: '/meetingroom', name: 'Meetingroom', component: Meetingroom,meta:{breadNumber:1}},
+        {path: '/order', name: 'Order', component: Order,meta:{breadNumber:2}},
       ]
     }
     ]
